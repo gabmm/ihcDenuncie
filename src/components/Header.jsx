066b7045b32cff9f742bf2ef98e67a042aaeeafc
Matheus Peron <matheus.peron@estudante.ufjf.br>
2024-09-05 13:48:11 -0300

    Implemetando o link entre páginas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return(
         <>
             <header className='cabecalho'>
-                <a href=''>
                     <img 
                         src="./src/assets/logo_escudo.png"
                         alt="logo do escudo"
                         className="logo"
                     />
-                </a>
                 <h1 id="page-title">Sistema de Denúncia de Vulnerabilidade Social</h1>
                 <nav>
-                    <a href= "#home" className="nav-link">Home</a>
-                    <a href= "#report" className="nav-link">Denunciar</a>
-                    <a href= "#resources" className="nav-link">Recursos</a>
-                    <a href= "#contact" className="nav-link">Contato</a>
+                    <Link to="/" className="nav-link">Home</Link>
+                    <Link to="/report" className="nav-link">Denunciar</Link>
                 </nav>
             </header>
         </>
